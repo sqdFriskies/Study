57700b5fd33131a40f6833f4068d63d580f12e17
Maksym Zakharov <maksym.zakharov@student.tuke.sk>
2024-12-07 15:50:08 +0100

    not working diagonal

--- ps_4/c4.c
+++ ps_4/c4.c
@@ -3,36 +3,191 @@
 #include <curses.h>
 void initialize_board(int rows, int cols, char board[rows][cols])
 {
-    for (int i = 0; i < rows;i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols;j++)
+        for (int j = 0; j < cols; j++)
         {
-            board[rows][cols] = ' ';
+            board[i][j] = '.';
         }
     }
 }
 
-// void print_board(int rows, int cols, const char board[rows][cols])
-// {
+void print_board(int rows, int cols, const char board[rows][cols])
+{
+    // Print the top border line
+    for (int i = 0; i < cols; i++)
+    {
+        printf("+---");
+    }
+    printf("+\n");
 
-// }
+    // Print the game board
+    for (int i = 0; i < rows; i++)
+    {
+        printf("|");
+        for (int j = 0; j < cols; j++)
+        {
+            printf(" %c |", board[i][j]);
+        }
+        printf("\n");
+    }
 
-// int is_valid_move(int rows, int cols, const char board[rows][cols], int col)
-// {
+    // Print the border line
+    for (int i = 0; i < cols; i++)
+    {
+        printf("+---");
+    }
+    printf("+\n");
 
-// }
+    // Print column numbers
+    for (int i = 0; i < cols; i++)
+    {
+        printf("  %d ", i + 1);
+    }
+    printf("\n");
+}
+int is_valid_move(int rows, int cols, const char board[rows][cols], int col)
+{
+    if(col-1 < 0 || col-1 > cols)
+    {
+        return false;
+    }
 
-// int drop_piece(int rows, int cols, char board[rows][cols], int col, char player_piece)
-// {
+    bool switcher = false;
+    
+    for (int i = rows - 1; i >= 0; i--)
+    {
+        if (board[i][col - 1] == '.')
+        {
+            switcher = true;
+            break;
+        }
+    }
+    return switcher;
+}
 
-// }
+int drop_piece(int rows, int cols, char board[rows][cols], int col, char player_piece)
+{
 
-// int check_win(int rows, int cols, const char board[rows][cols], int row, int col, char player_piece)
-// {
+    for (int i = rows - 1; i >= 0; i--)
+    {
+        if (board[i][col - 1] == '.')
+        {
+            board[i][col - 1] = player_piece;
+            return 1;
+        }
+    }
+    return -1;
+}
 
-// }
+int count_diagonal(int rows, int cols, const char board[rows][cols], int row, int col, char player_piece, int delta_r, int delta_c)
+{
+    int count = 0;
+    {
+        int r = row;
+        int c = col;
 
-// int is_board_full(int rows, int cols, const char board[rows][cols])
-// {
+        for (; ((delta_r >= 0) ? r < rows : r >= 0) &&
+               ((delta_c >= 0) ? c < cols : c >= 0) &&
+               (board[r][c] == player_piece); r+=delta_r, c+=delta_c)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
-// }
+int check_win(int rows, int cols, const char board[rows][cols], int row, int col, char player_piece)
+{
+    int count = 0;
+    count += count_diagonal(rows, cols, board, row, col, player_piece, +1, 0);
+    count += count_diagonal(rows, cols, board, row, col, player_piece, -1, 0);
+    if (count - 1 == rows)
+    {
+        return 1;
+    }
+    count = 0;
+    count += count_diagonal(rows, cols, board, row, col, player_piece, 0, -1);
+    count += count_diagonal(rows, cols, board, row, col, player_piece, 0, +1);
+    if (count - 1 == rows)
+    {
+        return 1;
+    }
+    
+    count = 0;
+    count += count_diagonal(rows, cols, board, row, col, player_piece, -1, +1);
+    count += count_diagonal(rows, cols, board, row, col, player_piece, +1, -1);
+    if (count - 1 == rows)
+    {
+        return 1;
+    }
+
+    count = 0;
+    count += count_diagonal(rows, cols, board, row, col, player_piece, -1, -1);
+    count += count_diagonal(rows, cols, board, row, col, player_piece, +1, +1);
+    if (count - 1 == rows)
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+
+int is_board_full(int rows, int cols, const char board[rows][cols])
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (board[i][j] == '.')
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+
+void run_c4()
+{
+    int rows = 4;
+    int cols = 4;
+    char board[rows][cols];
+    
+    bool is_player_x = true;
+    initialize_board(rows, cols, board);
+    
+    while (!is_board_full(rows, cols, board) )
+    {
+
+        const char player_piece = is_player_x ? 'X' : 'O';
+        is_player_x = !is_player_x;
+        int player_choice = -1;
+        // system("clear");
+        print_board(rows, cols, board);
+        printf("\n");
+        printf("\nPlayer %c: ", player_piece);
+        scanf("%d", &player_choice);
+        
+        if ( player_choice >= 0 && is_valid_move(rows, cols, board, player_choice))
+        {
+            if (drop_piece(rows, cols, board, player_choice, player_piece)>0)
+            {
+                if (check_win(rows, cols, board, 0, 0, player_piece))
+                {
+                    printf("\nYou win!\n");
+                    break;
+                }
+            }
+        }
+        else
+        {
+            printf("\nError: move is not valid. \nYou skip your turn.\n\n");
+        }
+
+        
+
+    }
+    print_board(rows, cols, board);
+}
--- ps_4/main.c
+++ ps_4/main.c
@@ -11,96 +11,102 @@ void print_line(const int rows, const int columns, char field[rows][columns], in
 void print_field(const int rows, const int columns, char field[rows][columns]);
 void generator(const int rows, const int columns, char field[rows][columns]);
 void down_possible(const int rows, const int columns, char field[rows][columns], int x, int y);
+void run_c4();
+
 int main()
 {
-    // const int rows = 3;
-    // const int columns = 4;
+    {
+        // const int rows = 3;
+        // const int columns = 4;
 
-    // char field[rows][columns];
+        // char field[rows][columns];
 
-    // generator(rows, columns, field); // ПРОВЕРКА ДЛЯ 1-ГО ЗАДАНИЯ
+        // generator(rows, columns, field); // ПРОВЕРКА ДЛЯ 1-ГО ЗАДАНИЯ
 
-    // const int rows = 8, columns = 6;
-    // char field[rows][columns];
+        // const int rows = 8, columns = 6;
+        // char field[rows][columns];
 
-    // // Инициализация массива вручную
-    // char init_data[4][6] = {
-    //     {' ', ' ', '^', '@', '@', ' '},
-    //     {'+', ' ', '^', '@', '^', ' '},
-    //     {'+', ' ', '*', '+', '+', ' '},
-    //     {'*', '+', '*', '@', '^', ' '}   // ПРОВЕРКА ДЛЯ 2 ЗАДАНИЯ
-    // };
+        // // Инициализация массива вручную
+        // char init_data[4][6] = {
+        //     {' ', ' ', '^', '@', '@', ' '},
+        //     {'+', ' ', '^', '@', '^', ' '},
+        //     {'+', ' ', '*', '+', '+', ' '},
+        //     {'*', '+', '*', '@', '^', ' '}   // ПРОВЕРКА ДЛЯ 2 ЗАДАНИЯ
+        // };
 
-    // for (int i = 0; i < rows; i++) {
-    //     for (int j = 0; j < columns; j++) {
-    //         field[i][j] = init_data[i][j];
-    //     }
-    // }
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < columns; j++) {
+        //         field[i][j] = init_data[i][j];
+        //     }
+        // }
 
-    // char init_data[8][6] = {
-    //     {' ', ' ', ' ', ' ', ' ', ' '},
-    //     {' ', '^', ' ', '^', ' ', '@'},
-    //     {' ', '*', '+', '*', ' ', '^'},
-    //     {' ', '+', '@', '*', ' ', '*'},
-    //     {' ', '@', '^', '^', ' ', '+'},
-    //     {' ', '@', '*', '+', ' ', '+'},
-    //     {' ', '+', '^', '@', ' ', '^'},
-    //     {' ', '@', '+', '@', ' ', '@'}}; // ПРОВЕРКА ДЛЯ 2 ЗАДАНИЯ
+        // char init_data[8][6] = {
+        //     {' ', ' ', ' ', ' ', ' ', ' '},
+        //     {' ', '^', ' ', '^', ' ', '@'},
+        //     {' ', '*', '+', '*', ' ', '^'},
+        //     {' ', '+', '@', '*', ' ', '*'},
+        //     {' ', '@', '^', '^', ' ', '+'},
+        //     {' ', '@', '*', '+', ' ', '+'},
+        //     {' ', '+', '^', '@', ' ', '^'},
+        //     {' ', '@', '+', '@', ' ', '@'}}; // ПРОВЕРКА ДЛЯ 2 ЗАДАНИЯ
 
-    // for (int i = 0; i < rows; i++)
-    // {
-    //     for (int j = 0; j < columns; j++)
-    //     {
-    //         field[i][j] = init_data[i][j];
-    //     }
-    // }
-    // game_field(rows, columns, field);
-    // down_possible(rows, columns, field, 4, 5);
-    // print_field(rows, columns, field);
+        // for (int i = 0; i < rows; i++)
+        // {
+        //     for (int j = 0; j < columns; j++)
+        //     {
+        //         field[i][j] = init_data[i][j];
+        //     }
+        // }
+        // game_field(rows, columns, field);
+        // down_possible(rows, columns, field, 4, 5);
+        // print_field(rows, columns, field);
 
-    // const int rows = 4;
-    // const int columns = 6;
+        // const int rows = 4;
+        // const int columns = 6;
 
-    //
-    // char field[4][6] = {
-    //     {'@', '+', ' ', ' ', ' ', ' '},
-    //     {'@', '+', ' ', ' ', ' ', ' '},
-    //     {'@', '+', ' ', ' ', ' ', ' '},
-    //     {'@', '+', ' ', ' ', ' ', ' '}
-    // };                                   // ПРОВЕРКА ДЛЯ 3 ЗАДАНИЯ
+        //
+        // char field[4][6] = {
+        //     {'@', '+', ' ', ' ', ' ', ' '},
+        //     {'@', '+', ' ', ' ', ' ', ' '},
+        //     {'@', '+', ' ', ' ', ' ', ' '},
+        //     {'@', '+', ' ', ' ', ' ', ' '}
+        // };                                   // ПРОВЕРКА ДЛЯ 3 ЗАДАНИЯ
 
-    //
-    // printf("Game field:\n");
-    // for (int i = 0; i < rows; i++) {
-    //     printf("%d |", i + 1);
-    //     for (int j = 0; j < columns; j++) {
-    //         printf(" %c |", field[i][j]);
-    //     }
-    //     printf("\n");
-    // }
+        //
+        // printf("Game field:\n");
+        // for (int i = 0; i < rows; i++) {
+        //     printf("%d |", i + 1);
+        //     for (int j = 0; j < columns; j++) {
+        //         printf(" %c |", field[i][j]);
+        //     }
+        //     printf("\n");
+        // }
 
-    //
-    // printf("---");
-    // for (int j = 0; j < columns; j++)
-    //{
-    //     printf("----");
-    // }
-    // printf("\n   ");
+        //
+        // printf("---");
+        // for (int j = 0; j < columns; j++)
+        //{
+        //     printf("----");
+        // }
+        // printf("\n   ");
 
-    // // Print the column numbers
-    // for (int j = 0; j < columns; j++)
-    //{
-    //     printf(" %d  ", j + 1);
-    // }
-    // printf("\n");
+        // // Print the column numbers
+        // for (int j = 0; j < columns; j++)
+        //{
+        //     printf(" %d  ", j + 1);
+        // }
+        // printf("\n");
 
-    // // Call the check function and display the result
-    // if (check(rows, columns, field))
-    //{
-    //     printf("All columns are fully collected (true).\n");
-    // } else {
-    //     printf("Some columns are incomplete or have mixed symbols (false).\n");
-    // }
-    ball_sort_puzzle();
+        // // Call the check function and display the result
+        // if (check(rows, columns, field))
+        //{
+        //     printf("All columns are fully collected (true).\n");
+        // } else {
+        //     printf("Some columns are incomplete or have mixed symbols (false).\n");
+        // }
+    }
+    // system("clear");
+    run_c4();
+
     return 0;
 }
