1e92ef60e231a0409dd782a7766627af947b1a0e
Maksym Zakharov <maksym.zakharov@student.tuke.sk>
2024-12-11 21:38:22 +0100

    ps_5_begin

--- ps_4/main.c
+++ ps_4/main.c
@@ -1,113 +1,113 @@
-#include <math.h>
-#include <curses.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "ballsortpuzzle.h"
-#include "c4.h"
 #include <stdbool.h>
-
-void print_line(const int rows, const int columns, char field[rows][columns], int row);
-void print_field(const int rows, const int columns, char field[rows][columns]);
-void generator(const int rows, const int columns, char field[rows][columns]);
-void down_possible(const int rows, const int columns, char field[rows][columns], int x, int y);
-void run_c4();
-
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+void encode_char(const char character, bool bits[8]);
+char decode_byte(const bool bits[8]);
+void encode_string(const char string[], bool bytes[strlen(string) + 1][8]);
+void decode_bytes(const int rows, bool bytes[rows][8], char string[rows]);
+void bytes_to_blocks(const int cols, const int offset, bool blocks[offset * 8][cols], const int rows, bool bytes[rows][8]);
+void blocks_to_bytes(const int cols, const int offset, bool blocks[offset * 8][cols], const int rows, bool bytes[rows][8]);
 int main()
 {
+    bool bits1[8];
+    encode_char('A', bits1);
+    for (int i = 0; i < 8; i++)
     {
-        // const int rows = 3;
-        // const int columns = 4;
-
-        // char field[rows][columns];
-
-        // generator(rows, columns, field); // test of task_1
-
-        // const int rows = 8, columns = 6;
-        // char field[rows][columns];
-
-        // // Инициализация массива вручную
-        // char init_data[4][6] = {
-        //     {' ', ' ', '^', '@', '@', ' '},
-        //     {'+', ' ', '^', '@', '^', ' '},
-        //     {'+', ' ', '*', '+', '+', ' '},
-        //     {'*', '+', '*', '@', '^', ' '}   // test of task_2
-        // };
-
-        // for (int i = 0; i < rows; i++) {
-        //     for (int j = 0; j < columns; j++) {
-        //         field[i][j] = init_data[i][j];
-        //     }
-        // }
-
-        // char init_data[8][6] = {
-        //     {' ', ' ', ' ', ' ', ' ', ' '},
-        //     {' ', '^', ' ', '^', ' ', '@'},
-        //     {' ', '*', '+', '*', ' ', '^'},
-        //     {' ', '+', '@', '*', ' ', '*'},
-        //     {' ', '@', '^', '^', ' ', '+'},
-        //     {' ', '@', '*', '+', ' ', '+'},
-        //     {' ', '+', '^', '@', ' ', '^'},
-        //     {' ', '@', '+', '@', ' ', '@'}}; // ПРОВЕРКА ДЛЯ 2 ЗАДАНИЯ
-
-        // for (int i = 0; i < rows; i++)
-        // {
-        //     for (int j = 0; j < columns; j++)
-        //     {
-        //         field[i][j] = init_data[i][j];
-        //     }
-        // }
-        // game_field(rows, columns, field);
-        // down_possible(rows, columns, field, 4, 5);
-        // print_field(rows, columns, field);
-
-        // const int rows = 4;
-        // const int columns = 6;
-
-        //
-        // char field[4][6] = {
-        //     {'@', '+', ' ', ' ', ' ', ' '},
-        //     {'@', '+', ' ', ' ', ' ', ' '},
-        //     {'@', '+', ' ', ' ', ' ', ' '},
-        //     {'@', '+', ' ', ' ', ' ', ' '}
-        // };                                   // ПРОВЕРКА ДЛЯ 3 ЗАДАНИЯ
-
-        //
-        // printf("Game field:\n");
-        // for (int i = 0; i < rows; i++) {
-        //     printf("%d |", i + 1);
-        //     for (int j = 0; j < columns; j++) {
-        //         printf(" %c |", field[i][j]);
-        //     }
-        //     printf("\n");
-        // }
-
-        //
-        // printf("---");
-        // for (int j = 0; j < columns; j++)
-        //{
-        //     printf("----");
-        // }
-        // printf("\n   ");
-
-        // // Print the column numbers
-        // for (int j = 0; j < columns; j++)
-        //{
-        //     printf(" %d  ", j + 1);
-        // }
-        // printf("\n");
-
-        // // Call the check function and display the result
-        // if (check(rows, columns, field))
-        //{
-        //     printf("All columns are fully collected (true).\n");
-        // } else {
-        //     printf("Some columns are incomplete or have mixed symbols (false).\n");
-        // }
+        printf("%d", bits1[i]);
     }
-    // system("clear");
+    printf("\n");
+    // prints: 01000001
 
-    //ball_sort_puzzle();
-    run_c4();
-    return 0;
+    bool bits2[8] = {0, 1, 0, 0, 0, 0, 0, 1};
+    printf("%c\n", decode_byte(bits2));
+    // prints: A
+}
+
+int power(int base, int exp)
+{
+    int result = 1;
+    for (int i = 0; i < exp; i++)
+    {
+        result *= base;
+    }
+    return result;
+}
+void encode_char(const char character, bool bits[8])
+{
+    for (int i = 0; i < 8; i++)
+    {
+        const bool bit = (character >> (i)) & 0x1;
+        bits[7 - i] = bit;
+    }
+}
+
+char decode_byte(const bool bits[8])
+{
+    char result = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        const char bit = bits[i] ? 1 : 0;
+        result = result | (bit << (7-i));
+    }
+    return result;
+}
+
+void encode_string(const char string[], bool bytes[strlen(string) + 1][8])
+{
+    int len = strlen(string) + 1;
+    for (int i = 0; i < len; i++)
+    {
+        int ascii_value = (int)string[i];
+        for (int j = 7; j >= 0; j--)
+        {
+            int divisor = power(2, j);
+            bytes[i][7 - j] = ascii_value / divisor;
+            ascii_value %= divisor;
+        }
+    }
+}
+void decode_bytes(const int rows, bool bytes[rows][8], char string[rows])
+{
+    for (int i = 0; i < rows; i++)
+    {
+        int ascii_value = 0;
+        for (int j = 0; j < 8; j++)
+        {
+            if (bytes[i][j])
+            {
+                ascii_value += power(2, 7 - j);
+            }
+        }
+        string[i] = (char)ascii_value;
+    }
+}
+void bytes_to_blocks(const int cols, const int offset, bool blocks[offset * 8][cols], const int rows, bool bytes[rows][8])
+{
+    int block_row, block_col;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            block_row = (i / cols) * 8 + j;
+            block_col = i % cols;
+            blocks[block_row][block_col] = bytes[i][j];
+        }
+    }
+}
+
+void blocks_to_bytes(const int cols, const int offset, bool blocks[offset * 8][cols], const int rows, bool bytes[rows][8])
+{
+    int block_row, block_col;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            block_row = (i / cols) * 8 + j;
+            block_col = i % cols;
+            bytes[i][j] = blocks[block_row][block_col];
+        }
+    }
 }
