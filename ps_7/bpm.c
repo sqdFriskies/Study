fbd41841890f0e89cc117971acbf24cc20a460e6
Maksym Zakharov <maksym.zakharov@student.tuke.sk>
2025-02-27 13:34:39 +0100

    task 1,2,3

--- ps_7/bpm.c
+++ ps_7/bpm.c
@@ -2,3 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+char *reverse(const char *text)
+{
+    if (text == NULL)
+    {
+        return NULL;
+    }
+
+    size_t len = strlen(text);
+    char *reversed = malloc(len + 1);
+    if (reversed == NULL)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < len; i++)
+    {
+        reversed[i] = toupper(text[len - 1 - i]);
+    }
+
+    reversed[len] = '\0';
+    return reversed;
+}
+
+int main()
+{
+
+    char *reversed = reverse("Hello world!");
+    printf("%s\n", reversed);
+    // "!DLROW OLLEH"
+    return 0;
+}
--- ps_7/playfair.c
+++ ps_7/playfair.c
@@ -2,276 +2,126 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "playfair.h"
-void build_matrix(const char *key, char matrix[5][5])
+
+void genMatrix(const char *k, char m[5][5], int p[26][2])
 {
-    int used[26] = {0};
-    int r = 0, c = 0;
-    // Сначала буквы из ключа (пробелы игнорируются, W заменяется на V)
-    for (int i = 0; key[i] != '\0'; i++)
-    {
-        if (isspace(key[i]))
-            continue;
-        if (!isalpha(key[i]))
-            continue; // если встречается недопустимый символ
-        char ch = toupper(key[i]);
-        if (ch == 'W')
-            ch = 'V';
-        int idx = ch - 'A';
-        if (!used[idx])
+    int u[26] = {0}, r = 0, c = 0;
+    for (; *k; k++)
+        if (!isspace(*k))
         {
-            used[idx] = 1;
-            matrix[r][c] = ch;
-            c++;
-            if (c == 5)
+            char ch = toupper(*k);
+            if (ch >= 'A' && ch <= 'Z' && ch != 'W')
             {
-                r++;
-                c = 0;
+                int i = ch - 'A';
+                if (!u[i])
+                {
+                    u[i] = 1;
+                    m[r][c] = ch;
+                    p[i][0] = r;
+                    p[i][1] = c;
+                    c = (c + 1) % 5;
+                    r += !c;
+                }
             }
-            if (r == 5)
-                break;
         }
-    }
-    // Заполняем оставшиеся клетки оставшимися буквами (W пропускаем)
     for (char ch = 'A'; ch <= 'Z'; ch++)
-    {
-        if (ch == 'W')
-            continue;
-        int idx = ch - 'A';
-        if (!used[idx])
+        if (ch != 'W')
         {
-            used[idx] = 1;
-            matrix[r][c] = ch;
-            c++;
-            if (c == 5)
+            int i = ch - 'A';
+            if (!u[i])
             {
-                r++;
-                c = 0;
-            }
-            if (r == 5)
-                break;
-        }
-    }
-}
-
-// Функция для поиска позиции буквы в матрице
-void find_position(char matrix[5][5], char letter, int *row, int *col)
-{
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if (matrix[i][j] == letter)
-            {
-                *row = i;
-                *col = j;
-                return;
+                m[r][c] = ch;
+                p[i][0] = r;
+                p[i][1] = c;
+                c = (c + 1) % 5;
+                r += !c;
             }
         }
-    }
 }
 
-// Функция для предварительной обработки текста при шифровании:
-// - удаляет пробелы, приводит к верхнему регистру,
-// - заменяет 'W' на 'V',
-// - вставляет 'X' между двумя подряд одинаковыми буквами (если они не равны 'X'),
-// - добавляет 'X' в конец, если длина нечётная.
-char *preprocess_text(const char *text)
+char *prepText(const char *t)
 {
-    int len = 0;
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-        if (!isspace(text[i]))
-            len++;
-    }
-    // Выделяем память, предполагая в худшем случае удвоение символов
-    char *temp = malloc(2 * len + 1);
-    if (!temp)
+    size_t l = 0;
+    for (const char *p = t; *p; p++)
+        if (!isspace(*p))
+            l++;
+    char *s = malloc(2 * l + 1), prv = 0;
+    if (!s)
         return NULL;
-    int j = 0;
-    char prev = '\0';
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-        if (isspace(text[i]))
-            continue;
-        char ch = toupper(text[i]);
-        if (!isalpha(ch))
-        { // если встречается недопустимый символ
-            free(temp);
-            return NULL;
-        }
-        if (ch == 'W')
-            ch = 'V';
-        // Если предыдущая буква равна текущей, вставляем 'X', если предыдущая не 'X'
-        if (prev == ch && ch != 'X')
+    size_t j = 0;
+    for (const char *p = t; *p; p++)
+        if (!isspace(*p))
         {
-            temp[j++] = 'X';
+            char ch = toupper(*p) == 'W' ? 'V' : toupper(*p);
+            if (!isalpha(ch))
+            {
+                free(s);
+                return NULL;
+            }
+            if (prv == ch && ch != 'X')
+                s[j++] = 'X';
+            s[j++] = ch;
+            prv = ch;
         }
-        temp[j++] = ch;
-        prev = ch;
-    }
-    // Если длина полученной строки нечётная, добавляем 'X'
-    if (j % 2 != 0)
-    {
-        temp[j++] = 'X';
-    }
-    temp[j] = '\0';
-    char *result = malloc(j + 1);
-    if (!result)
-    {
-        free(temp);
-        return NULL;
-    }
-    strcpy(result, temp);
-    free(temp);
-    return result;
+    if (j % 2)
+        s[j++] = 'X';
+    s[j] = 0;
+    char *r = strdup(s);
+    free(s);
+    return r;
 }
 
-// Функция шифрования по Playfair
-char *playfair_encrypt(const char *key, const char *text)
+char *crypt(const char *k, const char *t, int d)
 {
-    if (!key || !text)
+    if (!k || !t)
         return NULL;
-    // Проверка: текст может содержать только буквы и пробелы
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-        if (!isspace(text[i]) && !isalpha(text[i]))
+    for (const char *p = t; *p; p++)
+        if (!isspace(*p) && !isalpha(*p))
             return NULL;
-    }
-
-    char matrix[5][5];
-    build_matrix(key, matrix);
-
-    char *prep = preprocess_text(text);
-    if (!prep)
+    char m[5][5], *pt = prepText(t);
+    int p[26][2];
+    if (!pt)
         return NULL;
-    int len = strlen(prep);
-    int pair_count = len / 2;
-    // Для каждой пары: 2 буквы и пробел между парами (последняя пара без пробела)
-    int out_len = pair_count * 3;
-    char *output = malloc(out_len + 1);
-    if (!output)
+    genMatrix(k, m, p);
+    size_t l = strlen(pt), n = l / 2, pos = 0;
+    char *o = malloc(n * (d ? 2 : 3) + 1);
+    if (!o)
     {
-        free(prep);
+        free(pt);
         return NULL;
     }
-    int pos = 0;
-    for (int i = 0; i < len; i += 2)
+    for (size_t i = 0; i < l; i += 2)
     {
-        char a = prep[i];
-        char b = prep[i + 1];
-        int row1, col1, row2, col2;
-        find_position(matrix, a, &row1, &col1);
-        find_position(matrix, b, &row2, &col2);
-        char enc_a, enc_b;
-        if (row1 == row2)
+        int a = pt[i] - 'A', b = pt[i + 1] - 'A';
+        int r1 = p[a][0], c1 = p[a][1], r2 = p[b][0], c2 = p[b][1];
+        char ca, cb;
+        if (r1 == r2)
         {
-            // Если обе буквы в одной строке – сдвигаем вправо (циклически)
-            enc_a = matrix[row1][(col1 + 1) % 5];
-            enc_b = matrix[row2][(col2 + 1) % 5];
+            ca = m[r1][(c1 + (d ? 4 : 1)) % 5];
+            cb = m[r2][(c2 + (d ? 4 : 1)) % 5];
         }
-        else if (col1 == col2)
+        else if (c1 == c2)
         {
-            // Если в одном столбце – сдвигаем вниз
-            enc_a = matrix[(row1 + 1) % 5][col1];
-            enc_b = matrix[(row2 + 1) % 5][col2];
+            ca = m[(r1 + (d ? 4 : 1)) % 5][c1];
+            cb = m[(r2 + (d ? 4 : 1)) % 5][c2];
         }
         else
         {
-            // Если буквы на разных строках и столбцах – берем буквы на пересечениях
-            enc_a = matrix[row1][col2];
-            enc_b = matrix[row2][col1];
+            ca = m[r1][c2];
+            cb = m[r2][c1];
         }
-        output[pos++] = enc_a;
-        output[pos++] = enc_b;
-        if (i + 2 < len)
-        {
-            output[pos++] = ' ';
-        }
+        o[pos++] = ca;
+        o[pos++] = cb;
+        if (!d && i + 2 < l)
+            o[pos++] = ' ';
     }
-    output[pos] = '\0';
-    free(prep);
-    return output;
+    o[pos] = 0;
+    free(pt);
+    return o;
 }
 
-// Функция дешифрования по Playfair
-char *playfair_decrypt(const char *key, const char *text)
-{
-    if (!key || !text)
-        return NULL;
-    // Проверка: текст должен состоять только из букв и пробелов
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-        if (!isspace(text[i]) && !isalpha(text[i]))
-            return NULL;
-    }
-    // В примерах зашифрованный текст содержит буквы 'X', поэтому дополнительная проверка не делается.
-
-    char matrix[5][5];
-    build_matrix(key, matrix);
-
-    // Убираем пробелы из зашифрованного текста
-    int len = 0;
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-        if (!isspace(text[i]))
-            len++;
-    }
-    if (len % 2 != 0)
-        return NULL; // длина должна быть четной
-    char *prep = malloc(len + 1);
-    if (!prep)
-        return NULL;
-    int j = 0;
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-        if (!isspace(text[i]))
-        {
-            prep[j++] = toupper(text[i]);
-        }
-    }
-    prep[j] = '\0';
-
-    char *output = malloc(len + 1); // в расшифрованном тексте не будет пробелов
-    if (!output)
-    {
-        free(prep);
-        return NULL;
-    }
-    int pos = 0;
-    for (int i = 0; i < len; i += 2)
-    {
-        char a = prep[i];
-        char b = prep[i + 1];
-        int row1, col1, row2, col2;
-        find_position(matrix, a, &row1, &col1);
-        find_position(matrix, b, &row2, &col2);
-        char dec_a, dec_b;
-        if (row1 == row2)
-        {
-            // Если буквы в одной строке – сдвигаем влево
-            dec_a = matrix[row1][(col1 + 4) % 5];
-            dec_b = matrix[row2][(col2 + 4) % 5];
-        }
-        else if (col1 == col2)
-        {
-            // Если в одном столбце – сдвигаем вверх
-            dec_a = matrix[(row1 + 4) % 5][col1];
-            dec_b = matrix[(row2 + 4) % 5][col2];
-        }
-        else
-        {
-            // Если буквы на разных строках и столбцах – меняем столбцы
-            dec_a = matrix[row1][col2];
-            dec_b = matrix[row2][col1];
-        }
-        output[pos++] = dec_a;
-        output[pos++] = dec_b;
-    }
-    output[pos] = '\0';
-    free(prep);
-    return output;
-}
+char *playfair_encrypt(const char *k, const char *t) { return crypt(k, t, 0); }
+char *playfair_decrypt(const char *k, const char *t) { return crypt(k, t, 1); }
 
 int main()
 {
@@ -279,9 +129,12 @@ int main()
 
     // even length of string
     encrypted = playfair_encrypt("SeCReT", "Hello world");
-
+    printf("%s", encrypted);
+    // "Hello world" --> "HELXLOVORLDX"
+    // IS JZ JQ XN TK JC
     decrypted = playfair_decrypt("SeCReT", encrypted);
-
+    printf("%s", decrypted);
+    // HELXLOVORLDX
     free(encrypted);
     free(decrypted);
 
@@ -289,5 +142,11 @@ int main()
     encrypted = playfair_encrypt("world", "Hello");
     printf("%s", encrypted);
     // "Hello" --> "HELXLO"
+    // JB RY DR
+    decrypted = playfair_decrypt("world", encrypted);
+    printf("%s", decrypted);
+    // HELXLO
+    free(encrypted);
+    free(decrypted);
     return 0;
 }
